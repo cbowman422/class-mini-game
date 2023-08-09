9eae8139f8f1de61e47350da40b13b34245f4c3c
cbowman422 <chrisb0wman@hotmail.com>
2023-08-09 16:39:20 -0400

    survivor

---
+++ main.js
@@ -0,0 +1,41 @@
+const randomNum = (num) => {
+  return Math.floor(Math.random() * num)
+}
+
+class Survivor {
+  constructor(name){
+    this.name = name
+    this.campResource = [
+      {name: 'Fire', createResource: randomNum(3)},
+      {name: 'Water', createResource: randomNum(10)},
+      {name: 'Food', createResource: randomNum(5)}
+    ]
+    this.health = 100;
+    this.day = 0;
+  }
+
+  camp(damage){
+    const resource = randomNum(this.campResource.length);
+    const found = randomNum(this.campResource[resource].createResource);
+    this.health += found - damage
+    this.day += 1;
+
+    console.log(`There was ${this.campResource[resource].createResource} ${this.campResource[resource].name} locations and ${this.name} found ${found} health at ${this.health} %`)
+
+  }
+}
+
+const eric = new Survivor('eric');
+
+let gameOver = true;
+
+while (gameOver) {
+
+  eric.camp(100);
+
+  if (eric.health <= 0){
+    console.log(`lived for ${eric.day} days`);
+    gameOver = false;
+  }
+
+}
